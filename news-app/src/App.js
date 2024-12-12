ce3ab42aae519698a23c8fe2d6d1117cf1e67834
Maciej-entr <m.waszynski2005@wp.pl>
2024-12-13 00:37:21 +0100

    News-app added

---
+++ news-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import NewsBoard from './components/NewsBoard';
+import NewsItem from './components/NewsItem';
+
+function App() {
+  return (
+    <div className="App">
+<Navbar/>
+<NewsBoard/>
+
+    </div>
+  );
+}
+
+export default App;
