128255fb26b04048b103f662cc9e2039256ecf10
Maciej-entr <m.waszynski2005@wp.pl>
2024-12-18 19:13:27 +0100

    Adding a quizzer app

---
+++ quizz/src/App.js
@@ -0,0 +1,11 @@
+import Quizz from './components/quizz/Quizz';
+
+function App() {
+  return (
+    <div className="App">
+    <Quizz/>
+    </div>
+  );
+}
+
+export default App;
---
+++ quizz/src/components/assets/Config.jsx
@@ -0,0 +1,42 @@
+export const data = [
+    {
+        question: "How many elements are in the periodic table?",
+        option1: "124",
+        option2: "120",
+        option3: "116",
+        option4: "118",
+        ans: 4,
+    },
+    {
+        question: "Which planet in the Milky Way is the hottest?",
+        option1: "Earth",
+        option2: "Venus",
+        option3: "Sun",
+        option4: "Mercury",
+        ans: 2,
+    },
+    {
+        question: "Where is the strongest human muscle located?",
+        option1: "Leg",
+        option2: "Neck",
+        option3: "Butt",
+        option4: "Jaw",
+        ans: 4,
+    },
+    {
+        question: "What is the chemical element with the symbol Fe?",
+        option1: "Gold",
+        option2: "Silver",
+        option3: "Iron",
+        option4: "Uranium",
+        ans: 3,
+    },
+    {
+        question: "Which country drinks the most coffee per capita?",
+        option1: "Poland",
+        option2: "USA",
+        option3: "Finland",
+        option4: "Italy",
+        ans: 3,
+    }
+]
